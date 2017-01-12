8761d5b7835bd965bac4259ea35a0849e87b1e21
Unknown <remon_doo@hotmail.com>
2017-01-12 20:10:35 +0100

    css

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
   <title>VMX Hosting</title>
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="vmx.css">
+  <link rel="stylesheet" type="text/css" href="css/vmx.css">
   <script src="https://use.fontawesome.com/f288586ec5.js"></script>
 </head>
 <body>
