096ccf7c3dc5213903f644b477a4bf52f16826ce
Exhausting <ivodenhertog@gmail.com>
2017-01-16 15:19:55 +0100

    Fixed Navbar

--- nav-bar.php
+++ nav-bar.php
@@ -31,7 +31,7 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] == true){
 ';} 
 else{
   echo'
-  <nav class="navbar navbar-defaultn navbar-fixed-top">
+  <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
