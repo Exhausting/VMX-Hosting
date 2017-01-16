46127528b7653fc7c89cd8938dfb30ceb756c3f0
Exhausting <ivodenhertog@gmail.com>
2017-01-16 15:17:03 +0100

    Fixed Menu not folding.

--- nav-bar.php
+++ nav-bar.php
@@ -2,45 +2,69 @@
 
 //kijk als de sessie is gestart & kijk als de login sessie true is. info komt uit login.php
 if (isset($_SESSION["login"]) && $_SESSION["login"] == true){
-    echo'
-      <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="index.php">VMX</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="contact.php">Support</a></li>
-                            <li><a href="dashboard.php">Dashboard</a></li>
-                            <li><a href="logout.php">Logout</a></li>
-                    </ul>
-                </div>
-            </nav>
-    ';}
+  echo'
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">VMX</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="contact.php">Support</a></li>
+          <li><a href="dashboard.php">Dashboard</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div>
+  </nav>
+';} 
 else{
-    echo'
-        <nav class="navbar navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.php">VMX</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="signup.php">Register</a></li>
-                    <li><a href="contact.php">Support</a></li>
-                </ul>
-                <form method="post" action="login-sql.php" id="signin" class="navbar-form navbar-right" role="form">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                        <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">
-                    </div>            
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
-                    </div>
-                    <button type="submit" name="login" class="btn btn-primary">Login</button>
-                </form>
-            </div>
-        </nav>
-   ';}
+  echo'
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">VMX</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="signup.php">Register</a></li>
+          <li><a href="contact.php">Support</a></li>
+        </ul>
+        <form method="post" action="login-sql.php" id="signin" class="navbar-form navbar-right" role="form">
+          <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+            <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">
+          </div>            
+          <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+          </div>
+          <button type="submit" name="login" class="btn btn-primary">Login</button>
+        </form>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+';}
+
 ?>
