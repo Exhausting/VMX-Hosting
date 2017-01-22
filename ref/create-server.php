c128267c5bd5b17f519a68a2a70982be622ac033
Unknown <remon_doo@hotmail.com>
2017-01-22 23:55:01 +0100

    DB added/changed

--- ref/create-server.php
+++ ref/create-server.php
@@ -15,9 +15,17 @@ $vmname = $_POST["vm_name"];
 $operatingsystem = $_POST["operating_system"];
 $activationkey = $_POST["activation_key"];
 
+$stmt = $conn->prepare("insert into Customer_server (Email, Servicelevel, Cpu, Memory, Diskspace, Vmnaam, Operatingsytem, Activationkey) VALUES (?,?,?,?,?,?,?,?)");
 
+$stmt->bind_param("ssssssss", $email, $servicelevel, $cpu, $memory, $diskspace, $vmname, $operatingsystem, $activationkey);
 
-echo $email;
+$result = $stmt->execute();
+
+  if ($result === TRUE) {
+
+  header( "refresh:2;url=dashboard.php" );
+
+  }
 
 
 
