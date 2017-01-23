4c8b9ab33d5bd8a3ea22bf11834b71a90d5126f8
Unknown <remon_doo@hotmail.com>
2017-01-23 23:02:21 +0100

    lol

--- ref/head.php
+++ ref/head.php
@@ -8,4 +8,5 @@
   <script src="https://use.fontawesome.com/f288586ec5.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <link rel="icon" href="../images/favicon.png" type="image/png" sizes="16x16">
